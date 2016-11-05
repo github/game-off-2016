b20919a6a1ca9c7168db21370d23993cf0eaa1d2
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-11-05 23:20:41 +0700

    Move sprite creation to class Her
    Ref #1

--- src/war_state.ts
+++ src/war_state.ts
@@ -2,15 +2,26 @@ import * as Phaser from "phaser";
 
 export class WarState extends Phaser.State {
 
-    private her: Phaser.Sprite;
+    private her: Her;
 
     preload() {
         this.game.load.image("her", "assets/spritesheets/main_char.png");
     }
 
     create() {
-        this.her = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "her");
-        this.her.anchor.setTo(0.5, 0.5);
+        this.her = Her.create(this.game);
+    }
+
+}
+
+class Her {
+
+    constructor(private sprite: Phaser.Sprite) { }
+
+    static create(game: Phaser.Game): Her {
+        let sprite = game.add.sprite(game.world.centerX, game.world.centerY, "her");
+        sprite.anchor.setTo(0.5, 0.5);
+        return new Her(sprite);
     }
 
 }
