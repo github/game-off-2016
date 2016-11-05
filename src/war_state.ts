098cfab9c4fa71dfc6a1ef3e46d370662262a446
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-11-06 00:42:17 +0700

    Fixed war_state compile error
    Ref #2

--- src/war_state.ts
+++ src/war_state.ts
@@ -14,13 +14,13 @@ export class WarState extends Phaser.State {
     }
 
     update() {
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) {
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
             this.her.moveRight();
-        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT) {
+        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
             this.her.moveLeft();
-        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP) {
+        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
             this.her.moveUp();
-        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN) {
+        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
             this.her.moveDown();
         }
     }
