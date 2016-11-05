372350eae4a28e4d69f000e1f94dc12c29322f1d
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-11-05 23:09:17 +0700

    Move main class to src
    Ref #1

--- f5.ts
+++ f5.ts
@@ -1,34 +1,7 @@
 /// <reference path="./typings/index.d.ts"/>
 
-import * as Phaser from "phaser";
+import { F5 } from "./src/f5";
 import { WarState } from "./src/war_state";
-
-const GAME_WIDTH = 1920;
-const GAME_HEIGHT = 1080;
-const GAME_ID = "f5-game";
-
-class F5 {
-
-    private game: Phaser.Game;
-
-    constructor() {
-        this.game = new Phaser.Game(
-            GAME_WIDTH,
-            GAME_HEIGHT,
-            Phaser.AUTO,
-            GAME_ID,
-            {
-                create: this.create,
-            }
-        );
-    }
-
-    create() {
-        this.game.state.add("war", WarState, false);
-        this.game.state.start("war");
-    }
-
-}
 
 
 window.onload = () => new F5();
