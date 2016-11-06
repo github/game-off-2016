a4e92fd344595be892b21b2008b5f34cb1d4c349
Rodrigo Claros <kageto@gmail.com>
2016-11-05 20:56:32 -0400

    Set project structure

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,22 +1,27 @@
-var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
+PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
+PIXI.DEFAULT_RENDER_OPTIONS.antialias = false;
+PIXI.DEFAULT_RENDER_OPTIONS.roundPixels = true;
+
+var renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor : 0x1099bb});
 document.body.appendChild(renderer.view);
 
 // create the root of the scene graph
 var stage = new PIXI.Container();
 
 // create a texture from an image path
-var texture = PIXI.Texture.fromImage('assets/basics/bunny.png');
-
+var texture = PIXI.Texture.fromImage('assets/basics/nin.png');
 // create a new Sprite using the texture
 var bunny = new PIXI.Sprite(texture);
 
 // center the sprite's anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
+bunny.anchor.x = 0;
+bunny.anchor.y = 0;
 
 // move the sprite to the center of the screen
 bunny.position.x = 200;
 bunny.position.y = 150;
+bunny.scale.x = 1;
+bunny.scale.y = 1;
 
 stage.addChild(bunny);
 
@@ -26,7 +31,7 @@ function animate() {
     requestAnimationFrame(animate);
 
     // just for fun, let's rotate mr rabbit a little
-    bunny.rotation += 0.1;
+    // bunny.rotation += 0.1;
 
     // render the container
     renderer.render(stage);
