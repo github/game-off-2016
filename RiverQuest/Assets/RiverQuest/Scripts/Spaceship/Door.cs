e9fbd2bf06ad826f7d51169f4984b2f887922746
Shane Grüling <shane.grueling@portalwelten.de>
2016-11-08 21:32:46 +0100

    Added a test scene and the basic o2 system
    The spaceship test scene is to test the different systems until we get a
    concrete spaceship. Until than we can build our own spaceship and use it
    to test the different systems.
    
    I added a basic o2 system through doors. Each room is connected by
    doors. If the door is open it tries to get the same amount of oxygen in
    both rooms through adding and deducting oxygen.

---
+++ RiverQuest/Assets/RiverQuest/Scripts/Spaceship/Door.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace RiverQuest.Spaceship
+{
+    public class Door : MonoBehaviour
+    {
+        public Room Room1;
+        public Room Room2;
+
+        public bool IsOpen;
+
+        private float _timeSinceOpen;
+        private static float TimePerQxygenExchange = 0.5f;
+
+        public void Start()
+        {
+            if (Room1 == null && Room2 == null) throw new Exception("No room set for door " + name + ".");
+        }
+
+        // Update is called once per frame
+        public void FixedUpdate()
+        {
+            if(IsOpen)
+            {
+                ExchangeOxygen(Time.fixedDeltaTime);
+            }
+        }
+
+        private void ExchangeOxygen(float seconds)
+        {
+            _timeSinceOpen += seconds;
+
+            if(_timeSinceOpen < TimePerQxygenExchange)
+            {
+                return;
+            }
+
+            _timeSinceOpen -= TimePerQxygenExchange;
+
+            if(Room1 == null)
+            {
+                Room2.Oxygen--;
+                return;
+            }
+
+            if(Room2 == null)
+            {
+                Room1.Oxygen--;
+                return;
+            }
+
+            if (Room1.Oxygen == Room2.Oxygen) return;
+
+            if(Room1.Oxygen > Room2.Oxygen)
+            {
+                Room1.Oxygen--;
+                Room2.Oxygen++;
+            }
+            else
+            {
+                Room1.Oxygen++;
+                Room2.Oxygen--;
+            }
+        }
+    }
+}
---
+++ RiverQuest/Assets/RiverQuest/Scripts/Spaceship/Room.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RiverQuest.Spaceship
+{
+    public class Room : MonoBehaviour
+    {
+        public int Oxygen;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
