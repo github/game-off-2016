ef4a299928e01dbb9467fcf960fcf64c76e72be8
JamieRL <lee.jamr@gmail.com>
2016-11-07 22:15:12 -0800

    Initial commit

---
+++ Assets/Plugins/Editor/Rider/RiderAssetPostprocessor.cs
@@ -0,0 +1,93 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using UnityEditor;
+
+namespace Assets.Plugins.Editor.JetBrains
+{
+  public class RiderAssetPostprocessor : AssetPostprocessor
+  {
+    public static void OnGeneratedCSProjectFiles()
+    {
+      if (!RiderPlugin.Enabled)
+        return;
+      var currentDirectory = Directory.GetCurrentDirectory();
+      var projectFiles = Directory.GetFiles(currentDirectory, "*.csproj");
+
+      foreach (var file in projectFiles)
+      {
+        UpgradeProjectFile(file);
+      }
+    }
+
+    private static void UpgradeProjectFile(string projectFile)
+    {
+      RiderPlugin.Log(string.Format("Post-processing {0}", projectFile));
+      var doc = XDocument.Load(projectFile);
+      var projectContentElement = doc.Root;
+      XNamespace xmlns = projectContentElement.Name.NamespaceName; // do not use var
+
+      FixTargetFrameworkVersion(projectContentElement, xmlns);
+      SetLangVersion(projectContentElement, xmlns);
+
+      SetXCodeDllReference("UnityEditor.iOS.Extensions.Xcode.dll", xmlns, projectContentElement);
+      SetXCodeDllReference("UnityEditor.iOS.Extensions.Common.dll", xmlns, projectContentElement);
+
+      doc.Save(projectFile);
+    }
+
+    // Helps resolve System.Linq under mono 4 - RIDER-573
+    private static void FixTargetFrameworkVersion(XElement projectElement, XNamespace xmlns)
+    {
+      if (RiderPlugin.IsWindows)
+        return;
+
+      var targetFrameworkVersion = projectElement.Elements(xmlns + "PropertyGroup").
+        Elements(xmlns + "TargetFrameworkVersion").First();
+      var version = new Version(targetFrameworkVersion.Value.Substring(1));
+      if (version < new Version(4, 5))
+        targetFrameworkVersion.SetValue("v4.5");
+    }
+
+    private static void SetLangVersion(XElement projectElement, XNamespace xmlns)
+    {
+      // Add LangVersion to the .csproj. Unity doesn't generate it (although VSTU does).
+      // Not strictly necessary, as the Unity plugin for Rider will work it out, but setting
+      // it makes Rider work if it's not installed.
+      projectElement.AddFirst(new XElement(xmlns + "PropertyGroup",
+        new XElement(xmlns + "LangVersion", GetLanguageLevel())));
+    }
+
+    private static string GetLanguageLevel()
+    {
+      // https://bitbucket.org/alexzzzz/unity-c-5.0-and-6.0-integration/src
+      if (Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "CSharp70Support")))
+        return "7";
+      if (Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "CSharp60SUpport")))
+        return "6";
+
+      // Unity 5.5 supports C# 6, but only when targeting .NET 4.6. The enum doesn't exist pre Unity 5.5
+      if ((int)PlayerSettings.apiCompatibilityLevel >= 3)
+        return "6";
+
+      return "4";
+    }
+
+    private static void SetXCodeDllReference(string name, XNamespace xmlns, XElement projectContentElement)
+    {
+      var xcodeDllPath = Path.Combine("C:/Program Files/Unity/Editor/Data/PlaybackEngines/iOSSupport", name);
+      if (!File.Exists(xcodeDllPath))
+        xcodeDllPath = Path.Combine("/Applications/Unity/PlaybackEngines/iOSSupport", name);
+      if (File.Exists(xcodeDllPath))
+      {
+        var itemGroup = new XElement(xmlns + "ItemGroup");
+        var reference = new XElement(xmlns + "Reference");
+        reference.Add(new XAttribute("Include", Path.GetFileNameWithoutExtension(xcodeDllPath)));
+        reference.Add(new XElement(xmlns + "HintPath", xcodeDllPath));
+        itemGroup.Add(reference);
+        projectContentElement.Add(itemGroup);
+      }
+    }
+  }
+}
---
+++ Assets/Plugins/Editor/Rider/RiderPlugin.cs
@@ -0,0 +1,237 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Assets.Plugins.Editor.JetBrains
+{
+  [InitializeOnLoad]
+  public static class RiderPlugin
+  {
+    private static readonly string SlnFile;
+    private static readonly string DefaultApp = EditorPrefs.GetString("kScriptsDefaultApp");
+    public static readonly bool IsWindows;
+
+    internal static bool Enabled
+    {
+      get
+      {
+        if (string.IsNullOrEmpty(DefaultApp))
+          return false;
+        return DefaultApp.ToLower().Contains("rider"); // seems like .app doesn't exist as file
+      }
+    }
+
+    static RiderPlugin()
+    {
+      if (Enabled)
+      {
+        var riderFileInfo = new FileInfo(DefaultApp);
+        IsWindows = riderFileInfo.Extension == ".exe";
+
+        var newPath = riderFileInfo.FullName;
+        // try to search the new version
+
+        switch (riderFileInfo.Extension)
+        {
+          /*
+              Unity itself transforms lnk to exe
+              case ".lnk":
+              {
+                if (riderFileInfo.Directory != null && riderFileInfo.Directory.Exists)
+                {
+                  var possibleNew = riderFileInfo.Directory.GetFiles("*ider*.lnk");
+                  if (possibleNew.Length > 0)
+                    newPath = possibleNew.OrderBy(a => a.LastWriteTime).Last().FullName;
+                }
+                break;
+              }*/
+          case ".exe":
+          {
+            var possibleNew =
+              riderFileInfo.Directory.Parent.Parent.GetDirectories("*ider*")
+                .SelectMany(a => a.GetDirectories("bin"))
+                .SelectMany(a => a.GetFiles(riderFileInfo.Name))
+                .ToArray();
+            if (possibleNew.Length > 0)
+              newPath = possibleNew.OrderBy(a => a.LastWriteTime).Last().FullName;
+            break;
+          }
+          default:
+          {
+            break;
+          }
+        }
+        if (newPath != riderFileInfo.FullName)
+        {
+          Log(string.Format("Update {0} to {1}", riderFileInfo.FullName, newPath));
+          EditorPrefs.SetString("kScriptsDefaultApp", newPath);
+        }
+      }
+
+      var projectDirectory = Directory.GetParent(Application.dataPath).FullName;
+      var projectName = Path.GetFileName(projectDirectory);
+      SlnFile = Path.Combine(projectDirectory, string.Format("{0}.sln", projectName));
+    }
+
+    /// <summary>
+    /// Asset Open Callback (from Unity)
+    /// </summary>
+    /// <remarks>
+    /// Called when Unity is about to open an asset.
+    /// </remarks>
+    [UnityEditor.Callbacks.OnOpenAssetAttribute()]
+    static bool OnOpenedAsset(int instanceID, int line)
+    {
+      var riderFileInfo = new FileInfo(DefaultApp);
+      if (Enabled && (riderFileInfo.Exists || riderFileInfo.Extension == ".app"))
+      {
+        string appPath = Path.GetDirectoryName(Application.dataPath);
+
+        // determine asset that has been double clicked in the project view
+        var selected = EditorUtility.InstanceIDToObject(instanceID);
+
+        if (selected.GetType().ToString() == "UnityEditor.MonoScript" ||
+            selected.GetType().ToString() == "UnityEngine.Shader")
+        {
+          var assetFilePath = Path.Combine(appPath, AssetDatabase.GetAssetPath(selected));
+          var args = string.Format("{0}{1}{0} -l {2} {0}{3}{0}", "\"", SlnFile, line, assetFilePath);
+
+          CallRider(riderFileInfo.FullName, args);
+          return true;
+        }
+      }
+      return false;
+    }
+
+    private static void CallRider(string riderPath, string args)
+    {
+      var proc = new Process();
+      if (new FileInfo(riderPath).Extension == ".app")
+      {
+        proc.StartInfo.FileName = "open";
+        proc.StartInfo.Arguments = string.Format("-n {0}{1}{0} --args {2}", "\"", "/" + riderPath, args);
+        Log(proc.StartInfo.FileName + " " + proc.StartInfo.Arguments);
+      }
+      else
+      {
+        proc.StartInfo.FileName = riderPath;
+        proc.StartInfo.Arguments = args;
+        Log("\"" + proc.StartInfo.FileName + "\"" + " " + proc.StartInfo.Arguments);
+      }
+
+      proc.StartInfo.UseShellExecute = false;
+      proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      proc.StartInfo.CreateNoWindow = true;
+      proc.StartInfo.RedirectStandardOutput = true;
+      proc.Start();
+
+      if (new FileInfo(riderPath).Extension == ".exe")
+      {
+        try
+        {
+          ActivateWindow();
+        }
+        catch (Exception e)
+        {
+          Log("Exception on ActivateWindow: " + e);
+        }
+      }
+    }
+
+    private static void ActivateWindow()
+    {
+      var process = Process.GetProcesses().FirstOrDefault(b => !b.HasExited && b.ProcessName.Contains("Rider"));
+      if (process != null)
+      {
+        // Collect top level windows
+        var topLevelWindows = User32Dll.GetTopLevelWindowHandles();
+        // Get process main window title
+        var windowHandle = topLevelWindows.FirstOrDefault(hwnd => User32Dll.GetWindowProcessId(hwnd) == process.Id);
+        if (windowHandle != IntPtr.Zero)
+          User32Dll.SetForegroundWindow(windowHandle);
+      }
+    }
+
+    [MenuItem("Assets/Open C# Project in Rider", false, 1000)]
+    static void MenuOpenProject()
+    {
+      // Force the project files to be sync
+      SyncSolution();
+
+      // Load Project
+      CallRider(new FileInfo(DefaultApp).FullName, string.Format("{0}{1}{0}", "\"", SlnFile));
+    }
+
+    [MenuItem("Assets/Open C# Project in Rider", true, 1000)]
+    static bool ValidateMenuOpenProject()
+    {
+      return Enabled;
+    }
+
+    /// <summary>
+    /// Force Unity To Write Project File
+    /// </summary>
+    private static void SyncSolution()
+    {
+      System.Type T = System.Type.GetType("UnityEditor.SyncVS,UnityEditor");
+      System.Reflection.MethodInfo SyncSolution = T.GetMethod("SyncSolution",
+        System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+      SyncSolution.Invoke(null, null);
+    }
+
+    public static void Log(object message)
+    {
+      Debug.Log("[Rider] " + message);
+    }
+
+    static class User32Dll
+    {
+
+      /// <summary>
+      /// Gets the ID of the process that owns the window.
+      /// Note that creating a <see cref="Process"/> wrapper for that is very expensive because it causes an enumeration of all the system processes to happen.
+      /// </summary>
+      public static int GetWindowProcessId(IntPtr hwnd)
+      {
+        uint dwProcessId;
+        GetWindowThreadProcessId(hwnd, out dwProcessId);
+        return unchecked((int) dwProcessId);
+      }
+
+      /// <summary>
+      /// Lists the handles of all the top-level windows currently available in the system.
+      /// </summary>
+      public static List<IntPtr> GetTopLevelWindowHandles()
+      {
+        var retval = new List<IntPtr>();
+        EnumWindowsProc callback = (hwnd, param) =>
+        {
+          retval.Add(hwnd);
+          return 1;
+        };
+        EnumWindows(Marshal.GetFunctionPointerForDelegate(callback), IntPtr.Zero);
+        GC.KeepAlive(callback);
+        return retval;
+      }
+
+      public delegate Int32 EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+
+      [DllImport("user32.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+      public static extern Int32 EnumWindows(IntPtr lpEnumFunc, IntPtr lParam);
+
+      [DllImport("user32.dll", SetLastError = true)]
+      static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+      [DllImport("user32.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+      public static extern Int32 SetForegroundWindow(IntPtr hWnd);
+    }
+  }
+}
+
+// Developed using JetBrains Rider =)
