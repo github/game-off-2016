2d73d67c611a49a94781eafbc620037e803b3bd4
Chris Stevens <chris.stevens@surveysampling.com>
2016-11-08 20:38:03 -0500

    add RoomDetails script, attach script to room prefabs

---
+++ Assets/Scripts/RoomDetails.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomDetails : MonoBehaviour {
+    public int ID;
+    public int HorizontalSize;
+    public int VerticalSize;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
