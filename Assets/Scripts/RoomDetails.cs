4d7f806ad50fa8ec8db6557af771b3c773f5af41
Chris Stevens <chris.stevens@surveysampling.com>
2016-11-08 21:02:53 -0500

    add Door array to RoomDetails script

--- Assets/Scripts/RoomDetails.cs
+++ Assets/Scripts/RoomDetails.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 
 public class RoomDetails : MonoBehaviour {
-    public int ID;
+    public int Id;
     public int HorizontalSize;
     public int VerticalSize;
+    public GameObject[] Doors;
 
 	// Use this for initialization
 	void Start () {
