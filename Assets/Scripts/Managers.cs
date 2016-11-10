bf9bc5dac3f9602fb0d2c7bad5b3f88acccd10dc
Chris Stevens <chris.stevens@surveysampling.com>
2016-11-09 23:21:53 -0500

    add Managers script and static RoomNavigationManager reference

---
+++ Assets/Scripts/Managers.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Managers : MonoBehaviour {
+    public static RoomNavigationManager RoomNavigationManager { get; set; }
+
+	void Awake () {
+        RoomNavigationManager = GetComponent<RoomNavigationManager>();
+	}
+}
