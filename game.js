95b3158117717f71b768726616d501d6dd706803
fortythreesam <samdrugan@gmail.com>
2016-11-13 13:16:33 +0000

    Setup

--- game.js
+++ game.js
@@ -5,7 +5,28 @@
 	document.addEventListener('DOMContentLoaded', init, false);
   
     function init(){
-       
+		canvas = document.querySelector('canvas');
+        context = canvas.getContext('2d');
+		if (window.innerWidth-18< 1200){
+			canvas.width = 1200;
+		}
+		else if (window.innerWidth-18 > 2200){
+			canvas.width = 2200
+		}
+		else{
+			canvas.width = window.innerWidth - 18;
+		}
+		if (window.innerHeight-18< 800){
+			canvas.height = 800;
+		}
+		else if (window.innerHeight-18 > 1200){
+			canvas.height = 1200
+		}
+		else{
+			canvas.height = window.innerHeight - 18;
+		}
+        width = canvas.width;
+        height = canvas.height;
     }
 	
 	function main(){
