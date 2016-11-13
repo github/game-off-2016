e8a33100e41a8bec3537ad0f1ab26a0e7554cbdf
fortythreesam <samdrugan@gmail.com>
2016-11-13 15:09:33 +0000

    Typing

--- game.js
+++ game.js
@@ -2,6 +2,9 @@
 
 	var canvas
 	var context
+	var upcoming_words = []
+	var current_word 
+	var current_letter = 0
 	document.addEventListener('DOMContentLoaded', init, false);
   
     function init(){
@@ -27,14 +30,42 @@
 		}
         width = canvas.width;
         height = canvas.height;
+		for (var i = 0; i < 10; i += 1){
+			upcoming_words.push(words[getRandomNumber(0,67724)].toLowerCase())
+		}
+		current_word = upcoming_words[0]
+		window.setInterval(main,33);
+		window.addEventListener("keydown",controls,false);
     }
 	
 	function main(){
-		
+		if (current_letter === current_word.length){
+			nextWord()
+		}
+		draw()
 	}
 	
 	function draw(){
-		
+		context.clearRect(0,0,width,height)
+		context.fillStyle = "#555555"
+		context.textAlign = "center"
+		context.font = "112px Arial"
+		context.fillText(current_word,width/2,height/2)
+	}
+	
+	function controls(event){
+		keycode = event.keyCode
+		letter = String.fromCharCode(keycode)
+		if (letter.toLowerCase() == current_word.substring(current_letter,current_letter+1)){
+			current_letter += 1
+		}
+	}
+	
+	function nextWord(){
+		upcoming_words.splice(0,1)
+		upcoming_words.push(words[getRandomNumber(0,67724)].toLowerCase())
+		current_word = upcoming_words[0]
+		current_letter = 0
 	}
 	
     function getRandomNumber(min, max) {
