e9a3de85907eb96a5bb736f92b5c69a6c40e8536
fortythreesam <samdrugan@gmail.com>
2016-11-13 13:15:01 +0000

    Setup

---
+++ game.js
@@ -0,0 +1,24 @@
+(function (){
+
+	var canvas
+	var context
+	document.addEventListener('DOMContentLoaded', init, false);
+  
+    function init(){
+       
+    }
+	
+	function main(){
+		
+	}
+	
+	function draw(){
+		
+	}
+	
+    function getRandomNumber(min, max) {
+		return Math.floor(Math.random() * (max - min+1)) + min;
+    }
+	
+
+})(); 
