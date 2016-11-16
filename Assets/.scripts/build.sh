86ffc06846dbd0cf355299cfd761e84af085bdc9
Peter Colapietro <petercolapietro@gmail.com>
2016-11-16 06:03:45 -0500

    Parameterize logfile location and clean up echo statements

--- Assets/.scripts/build.sh
+++ Assets/.scripts/build.sh
@@ -3,41 +3,48 @@
 project="ci-build"
 
 LOGDATE=$(date +%Y-%m-%dT%H:%M:%S%z)
+LOGFILE=$(pwd)/unity-build-$LOGDATE.log
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity-build-$LOGDATE.log \
+  -logFile $LOGFILE \
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+echo 'Logs after Windows build'
+cat $LOGFILE
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity-build-$LOGDATE.log \
+  -logFile $LOGFILE \
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-echo 'Logs from OS X build'
-cat $(pwd)/unity.log
+echo 'Logs after OS X build'
+cat $LOGFILE
 
 echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity-build-$LOGDATE.log \
+  -logFile $LOGFILE \
   -projectPath $(pwd) \
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
+echo 'Logs after Linux build'
+cat $LOGFILE
+
 echo 'Logs from build'
-cat $(pwd)/unity.log
+cat $LOGFILE
