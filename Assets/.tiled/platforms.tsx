31e6a05b60a6da9334f4d23d7a9364deb26f76d1
Chris Stevens <munkeyxis@gmail.com>
2016-11-16 21:33:31 -0500

    add tiled files

---
+++ Assets/.tiled/platforms.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="platforms" tilewidth="128" tileheight="128" spacing="1" tilecount="4" columns="4">
+ <image source="C:/Users/Monkey/Desktop/platform sprites.png" width="515" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.333333" y="0.666667" width="128" height="74"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="52.6667" y="0.666667" width="74" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="52.6667" width="128" height="74"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="0.666667" y="0" width="74" height="128"/>
+  </objectgroup>
+ </tile>
+</tileset>
