0d33d3cf90c420999f4f423862abdfa310b46e8e
PabloDons <omar.saad0705@gmail.com>
2016-11-22 14:34:46 +0100

    Optimized client

--- Client/msclient.js
+++ Client/msclient.js
@@ -1,85 +1,43 @@
 var minesweeper = new ms(100, 15);
-var text = "";
-numOfFields=0;
-var fieldIcons = ["Bilder/0.png","Bilder/1.png","Bilder/2.png","Bilder/3.png","Bilder/4.png","Bilder/5.png","Bilder/6.png","Bilder/7.png","Bilder/8.png","Bilder/flagged.png","Bilder/default.png"];
+var msmap = [];
+//numOfFields=minesweeper.size*minesweeper.size; //unused
+//var fieldIcons = ["Bilder/0.png","Bilder/1.png","Bilder/2.png","Bilder/3.png","Bilder/4.png","Bilder/5.png","Bilder/6.png","Bilder/7.png","Bilder/8.png","Bilder/flagged.png","Bilder/default.png"];
 drawMap();
 function drawMap(){
-	text += "<table>";
+    var tableEl = document.createElement("table");
     for (var y=0; y<minesweeper.size; y++){
-		text += "<tr>";
-		for (var x=0; x<minesweeper.size; x++){
-			text += '<td data-x ="'+x+'" data-y="'+y+'"></td>';
-            numOfFields++;
-		}
-		text += "</tr>";
-	}
-	text += "</table";
-	document.getElementById("game").innerHTML = text;
-    for (var b = 0; b < minesweeper.size*minesweeper.size; b++) {
-        document.getElementsByTagName("td")[b].style.backgroundImage = "url('Bilder/default.png')";
+        var trEl = document.createElement("tr");
+        msmap.push([]);
+        for (var x=0; x<minesweeper.size; x++){
+            var tdEl = document.createElement("td");
+            tdEl.className="game-item";
+            tdEl.style["background-image"] = "url('bilder/default.png')";
+            tdEl.addEventListener("click",send);
+            trEl.appendChild(tdEl);
+            msmap[y].push(tdEl);
+        }
+        tableEl.appendChild(trEl);
     }
-    for (var i =0;i<numOfFields;i++){
-        var knapper = document.querySelectorAll("td");
-        knapper[i].addEventListener("click", send);
-    }
+    document.getElementById("game").appendChild(tableEl);
 
 }
 function send(e){
-    xCoord = Number(e.target.getAttribute("data-x"));
-    yCoord = Number(e.target.getAttribute("data-y"));
+    var Coords = e.target.id.replace(/^(ms)/,"").split("_");
 
-    updateMap(minesweeper.clickBox({x:xCoord, y:yCoord}));//flyttes til connection.onmessage eller hva det nå enn heter
+    console.log("updating map...");
+    updateMap(minesweeper.clickBox({x:Number(Coords[0]), y:Number(Coords[1])}));//flyttes til connection.onmessage eller hva det nå enn heter
+    console.log("done.");
 }
 function updateMap(theResultOfClickBox) {
-    if(theResultOfClickBox.hit==true){
+    if(theResultOfClickBox.hit===true){
         theGameHasEnded();
-
-    }else {
-        for (var i = 0; i < theResultOfClickBox.act.length; i++) {
-            xCoord = theResultOfClickBox.act[i].x;
-            yCoord = theResultOfClickBox.act[i].y;
-            for (var j = 0; j < minesweeper.size*minesweeper.size; j++) {
-                if (document.getElementsByTagName("td")[j].getAttribute("data-y")==yCoord&&document.getElementsByTagName("td")[j].getAttribute("data-x")==xCoord){
-                    var styleText = "";
-                    var t =theResultOfClickBox.act[i].numOfAdj;
-                    switch (t) {
-                        case 0:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/0.png')";
-                            break;
-                        case 1:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/1.png')";
-                            break;
-                        case 2:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/2.png')";
-                            break;
-                        case 3:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/3.png')";
-                            break;
-                        case 4:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/4.png')";
-                            break;
-                        case 5:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/5.png')";
-                            break;
-                        case 6:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/6.png')";
-                            break;
-                        case 7:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/7.png')";
-                            break;
-                        case 8:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/8.png')";
-                            break;
-                        default:
-                            document.getElementsByTagName("td")[j].style.backgroundImage = "url('Bilder/default.png')";
-                    }
-
-                    j=minesweeper.size*minesweeper.size;
-
-
-                }
-            }
-        }
+        return;
+    }
+    for (var i=0; i<theResultOfClickBox.act.length;i++) {
+        var x = theResultOfClickBox.act[i].x;
+        var y = theResultOfClickBox.act[i].y;
+        var t = theResultOfClickBox.act[i].numOfAdj;
+        msmap[y][x].style.backgroundImage="url('bilder/"+t+".png')";
     }
 }
 function theGameHasEnded(){
