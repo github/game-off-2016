416c07b9df151cb5e5052e1428fceccb7ea3f738
PabloDons <omar.saad0705@gmail.com>
2016-11-22 16:10:33 +0100

    Undid reset of msclient (thx hilmar)

--- Client/msclient.js
+++ Client/msclient.js
@@ -1,69 +1,51 @@
-var minesweeper = new ms(100, 15);
-var text = "";
-numOfFields = 0;
-drawMap();
-
-function drawMap() {
-    text += "<table>";
-    for (var y = 0; y < minesweeper.size; y++) {
-        text += "<tr>";
-        for (var x = 0; x < minesweeper.size; x++) {
-            text += '<td data-x ="' + x + '" data-y="' + y + '"></td>';
-            numOfFields++;
+var size = 100;
+var numBombs = 15;
+var minesweeper = new ms(size, numBombs);
+var msmap = drawMap();
+//numOfFields=size*size; //unused
+//var fieldIcons = ["Bilder/0.png","Bilder/1.png","Bilder/2.png","Bilder/3.png","Bilder/4.png","Bilder/5.png","Bilder/6.png","Bilder/7.png","Bilder/8.png","Bilder/flagged.png","Bilder/default.png"];
+function drawMap(getMapResult){
+    var msmap = [];
+    var tableEl = document.createElement("table");
+    for (var y=0; y<size; y++){
+        var trEl = document.createElement("tr");
+        msmap.push([]);
+        for (var x=0; x<size; x++){
+            var tdEl = document.createElement("td");
+            tdEl.className="game-item";
+            tdEl.style["background-image"] = "url('bilder/default.png')";
+            tdEl.setAttribute("data-x",""+x);
+            tdEl.setAttribute("data-y",""+y);
+            tdEl.addEventListener("click",send);
+            trEl.appendChild(tdEl);
+            msmap[y].push(tdEl);
         }
-        text += "</tr>";
+        tableEl.appendChild(trEl);
     }
-    text += "</table";
-    document.getElementById("game").innerHTML = text;
-    for (var b = 0; b < minesweeper.size * minesweeper.size; b++) {
-        document.getElementsByTagName("td")[b].style.backgroundImage =
-            "url('bilder/default.png')";
-    }
-    for (var i = 0; i < numOfFields; i++) {
-        var knapper = document.querySelectorAll("td");
-        knapper[i].addEventListener("click", send);
-        knapper[i].style.backgroundImage = "url('bilder/default.png')";
-    }
-
+    document.getElementById("game").appendChild(tableEl);
+    return msmap;
 }
+function send(e){
+    var xCoord = Number(e.target.getAttribute("data-x"));
+    var yCoord = Number(e.target.getAttribute("data-y"));
 
-function send(e) {
-    xCoord = Number(e.target.getAttribute("data-x"));
-    yCoord = Number(e.target.getAttribute("data-y"));
-
-    updateMap(minesweeper.clickBox({
-        x: xCoord,
-        y: yCoord
-    })); //flyttes til connection.onmessage eller hva det nå enn heter
+    var result = minesweeper.clickBox({x:xCoord, y:yCoord});
+    console.log("updating map...");
+    updateMap(result);//flyttes til connection.onmessage eller hva det nå enn heter
+    console.log("Map udated.");
 }
-
 function updateMap(theResultOfClickBox) {
-    if (theResultOfClickBox.hit == true) {
+    if(theResultOfClickBox.hit===true){
         theGameHasEnded();
-    } else {
-        for (var i = 0; i < theResultOfClickBox.act.length; i++) {
-            xCoord = theResultOfClickBox.act[i].x;
-            yCoord = theResultOfClickBox.act[i].y;
-            var tmptr = document.getElementsByTagName("tr");
-            for (var j = 0; j < tmptr.length; j += 1) {
-                if (tmptr[j].getAttribute("data-y") == yCoord) {
-                    var tmptd = tmptr[j].getElementsByTagName("td");
-                    for (var k = 0; k < minesweeper.size; k++) {
-                        if (tmptd[k].getAttribute("data-x") == xCoord) {
-
-                            var styleText = "";
-                            var t = theResultOfClickBox.act[i].numOfAdj;
-                            tmptd[j].style.backgroundImage = "url('bilder/'" +
-                                t + ".png')";
-                        }
-                    }
-                    break;
-                }
-            }
-        }
+        return;
+    }
+    for (var i=0; i<theResultOfClickBox.act.length;i++) {
+        var x = theResultOfClickBox.act[i].x;
+        var y = theResultOfClickBox.act[i].y;
+        var t = theResultOfClickBox.act[i].numOfAdj;
+        minesweeper.map[y][x].style.backgroundImage="url('bilder/"+t+".png')";
     }
 }
-
-function theGameHasEnded() {
+function theGameHasEnded(){
     console.log("ya lost");
 }
