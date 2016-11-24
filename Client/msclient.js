30123fed6339753f95573ddfc454de68419340a3
hilmar1 <Hilmar@Hilmars-MacBook-Pro.local>
2016-11-24 21:24:33 +0100

    ok

--- Client/msclient.js
+++ Client/msclient.js
@@ -1,22 +1,24 @@
 var size = 100;
-var numBombs = 15;
+var numBombs = 1500;
 var minesweeper = new ms(size, numBombs);
 var msmap = drawMap();
-//numOfFields=size*size; //unused
-//var fieldIcons = ["Bilder/0.png","Bilder/1.png","Bilder/2.png","Bilder/3.png","Bilder/4.png","Bilder/5.png","Bilder/6.png","Bilder/7.png","Bilder/8.png","Bilder/flagged.png","Bilder/default.png"];
-function drawMap(getMapResult){
+/*connection.onmessage = function {
+    updateMap()
+}*/
+
+function drawMap(getMapResult) {
     var msmap = [];
     var tableEl = document.createElement("table");
-    for (var y=0; y<size; y++){
+    for (var y = 0; y < size; y++) {
         var trEl = document.createElement("tr");
         msmap.push([]);
-        for (var x=0; x<size; x++){
+        for (var x = 0; x < size; x++) {
             var tdEl = document.createElement("td");
-            tdEl.className="game-item";
+            tdEl.className = "game-item";
             tdEl.style["background-image"] = "url('bilder/default.png')";
-            tdEl.setAttribute("data-x",""+x);
-            tdEl.setAttribute("data-y",""+y);
-            tdEl.addEventListener("click",send);
+            tdEl.setAttribute("data-x", "" + x);
+            tdEl.setAttribute("data-y", "" + y);
+            tdEl.addEventListener("click", send);
             trEl.appendChild(tdEl);
             msmap[y].push(tdEl);
         }
@@ -25,27 +27,41 @@ function drawMap(getMapResult){
     document.getElementById("game").appendChild(tableEl);
     return msmap;
 }
-function send(e){
+
+
+
+function send(e) {
     var xCoord = Number(e.target.getAttribute("data-x"));
     var yCoord = Number(e.target.getAttribute("data-y"));
 
-    var result = minesweeper.clickBox({x:xCoord, y:yCoord});
+    var result = minesweeper.clickBox({
+        x: xCoord,
+        y: yCoord
+    });
     console.log("updating map...");
-    updateMap(result);//flyttes til connection.onmessage eller hva det nå enn heter
+    updateMap(result); //flyttes til connection.onmessage eller hva det nå enn heter
     console.log("Map udated.");
+    //connection.send(result)
 }
+
 function updateMap(theResultOfClickBox) {
-    if(theResultOfClickBox.hit===true){
+    if (theResultOfClickBox.hit === true) {
         theGameHasEnded();
         return;
     }
-    for (var i=0; i<theResultOfClickBox.act.length;i++) {
+    for (var i = 0; i < theResultOfClickBox.act.length; i++) {
         var x = theResultOfClickBox.act[i].x;
         var y = theResultOfClickBox.act[i].y;
         var t = theResultOfClickBox.act[i].numOfAdj;
-        msmap[y][x].style.backgroundImage="url('bilder/"+t+".png')";
+        msmap[y][x].style.backgroundImage = "url('bilder/" + t + ".png')";
     }
 }
-function theGameHasEnded(){
+
+function theGameHasEnded() {
     console.log("ya lost");
 }
+//connection.onmessage
+function getMap() {
+    ms
+}
+//}
